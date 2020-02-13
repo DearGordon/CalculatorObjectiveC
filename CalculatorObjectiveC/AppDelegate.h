5fe9c40ab5f037c4500a3101acabdae4218bc89f
Gordon <hellogordon.f@gmail.com>
2020-02-13 14:07:20 +0800

    Initial Commit

---
+++ CalculatorObjectiveC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CalculatorObjectiveC
+//
+//  Created by Kuan-Chieh Feng on 2020/2/13.
+//  Copyright © 2020 Kuan-Chieh Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ CalculatorObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CalculatorObjectiveC
+//
+//  Created by Kuan-Chieh Feng on 2020/2/13.
+//  Copyright © 2020 Kuan-Chieh Feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
