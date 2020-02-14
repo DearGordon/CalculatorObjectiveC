5e222e0b678a0157c352aaa1d79a34e7a430f7fa
GordonFeng <hellogordon.f@gmail.com>
2020-02-14 18:15:06 +0800

    update

---
+++ CalculatorObjectiveC/Model.h
@@ -0,0 +1,17 @@
+//
+//  Model.h
+//  CalculatorObjectiveC
+//
+//  Created by Gordon Feng on 14/2/20.
+//  Copyright © 2020 Kuan-Chieh Feng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Model : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CalculatorObjectiveC/Model.m
@@ -0,0 +1,13 @@
+//
+//  Model.m
+//  CalculatorObjectiveC
+//
+//  Created by Gordon Feng on 14/2/20.
+//  Copyright © 2020 Kuan-Chieh Feng. All rights reserved.
+//
+
+#import "Model.h"
+
+@implementation Model
+
+@end
