8be6d70d8a557e0e1b80c7fb48c9f78d75866bfa
GordonFeng <hellogordon.f@gmail.com>
2020-02-14 18:19:59 +0800

    change to GordonFeng

--- CalculatorObjectiveC/Model.h
+++ CalculatorObjectiveC/Model.h
@@ -12,6 +12,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface Model : NSObject
 
+-(void)doOperation:(NSDecimalNumber*)leftNumber :(NSDecimalNumber*)rightNumber;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- CalculatorObjectiveC/Model.m
+++ CalculatorObjectiveC/Model.m
@@ -10,4 +10,6 @@
 
 @implementation Model
 
+
+
 @end
